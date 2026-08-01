f5c1d85dd35d270b8206c5fc06ad6b449d0ec737
Ramin <ramin.rezaie12345@gmail.com>
2026-08-01 09:47:57 -0700

    Add global config module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
+import { ConfigModule } from './common/config/config.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
