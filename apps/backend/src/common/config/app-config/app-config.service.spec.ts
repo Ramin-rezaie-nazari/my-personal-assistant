fa79ae621a37cda3dd7ba4d6d9daa72def676476
Ramin <ramin.rezaie12345@gmail.com>
2026-08-02 15:49:38 +0430

    test: fix service dependency test setup

--- apps/backend/src/common/config/app-config/app-config.service.spec.ts
+++ apps/backend/src/common/config/app-config/app-config.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
 import { AppConfigService } from './app-config.service';
 
 describe('AppConfigService', () => {
@@ -6,6 +7,7 @@ describe('AppConfigService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       providers: [AppConfigService],
     }).compile();
 
