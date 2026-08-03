2c44b28f1c0904d20f0c1eeca82b9e953b9c888f
Ramin <ramin.rezaie12345@gmail.com>
2026-08-03 10:14:39 +0430

    feat: add prisma module and service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './modules/health/health.module';
 import { ConfigModule } from './common/config/config.module';
+import { PrismaModule } from './common/database/prisma.module';
 
 @Module({
-  imports: [HealthModule, ConfigModule],
+  imports: [HealthModule, ConfigModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
