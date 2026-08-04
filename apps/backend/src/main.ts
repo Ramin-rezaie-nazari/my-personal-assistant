fafc6e7a2dbcb5592c8c4b71aeafb0afc4e7c9dc
Ramin <ramin.rezaie12345@gmail.com>
2026-08-04 11:30:08 +0430

    feat: enable global request validation pipeline

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './common/config/app-config/app-config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(AppConfigService);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(configService.port);
 }
+
 void bootstrap();
