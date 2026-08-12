bbcb9e329360b699e06ff99131654f62d9c3e54d
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-12 11:13:08 +0330

    feat(execution): register life execution module

---
+++ apps/backend/src/modules/life-execution/life-execution.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { LifeExecutionController } from './controllers/life-execution.controller';
+import { LifeExecutionService } from './services/life-execution.service';
+
+@Module({ controllers: [LifeExecutionController], providers: [LifeExecutionService], exports: [LifeExecutionService] })
+export class LifeExecutionModule {}
