26d52dfbd0cf5244099344673e88cc497d528f83
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-12 11:28:05 +0330

    feat(tasks): register life tasks module

---
+++ apps/backend/src/modules/life-tasks/life-tasks.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { LifeTasksController } from './controllers/life-tasks.controller';
+import { LifeTasksService } from './services/life-tasks.service';
+
+@Module({ controllers:[LifeTasksController], providers:[LifeTasksService], exports:[LifeTasksService] })
+export class LifeTasksModule {}
