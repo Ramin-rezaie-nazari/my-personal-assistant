9ba08e2ad227bed2c98b4b7f3bea52612d3a2fc6
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-12 10:25:10 +0330

    chore: remove premature smart notification migration from main

--- apps/backend/prisma/migrations/20260812102400_smart_notifications/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "Notification" ADD COLUMN "dedupeKey" TEXT NOT NULL DEFAULT 'legacy';
-ALTER TABLE "Notification" ADD COLUMN "priority" INTEGER NOT NULL DEFAULT 2;
-CREATE UNIQUE INDEX "Notification_userId_dedupeKey_key" ON "Notification"("userId", "dedupeKey");
