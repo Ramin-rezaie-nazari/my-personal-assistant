da34a1c97fd43e153f2dd090f5f635c7ae4713a4
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-12 08:11:51 +0330

    feat(brain): add weekly progress type

---
+++ apps/backend/src/modules/personal-brain/types/brain-weekly-progress.types.ts
@@ -0,0 +1,10 @@
+export type BrainWeeklyProgress = {
+  days: number;
+  daysWithLogs: number;
+  totalWaterMl: number;
+  averageWaterMl: number;
+  totalCalories: number;
+  averageCalories: number;
+  totalProtein: number;
+  averageProtein: number;
+};
