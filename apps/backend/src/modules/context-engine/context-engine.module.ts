0c170b5ab6cda813e77f6ee10f1bba18b17d7ded
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-13 07:24:34 +0330

    feat(context): register life context fusion

--- apps/backend/src/modules/context-engine/context-engine.module.ts
+++ apps/backend/src/modules/context-engine/context-engine.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { ContextEngineController } from './controllers/context-engine.controller';
 import { ContextEngineService } from './services/context-engine.service';
 import { ContextBuilderService } from './services/context-builder.service';
+import { LifeContextFusionService } from './services/life-context-fusion.service';
 
 @Module({
   controllers: [ContextEngineController],
-  providers: [ContextEngineService, ContextBuilderService],
-  exports: [ContextEngineService, ContextBuilderService],
+  providers: [ContextEngineService, ContextBuilderService, LifeContextFusionService],
+  exports: [ContextEngineService, ContextBuilderService, LifeContextFusionService],
 })
 export class ContextEngineModule {}
