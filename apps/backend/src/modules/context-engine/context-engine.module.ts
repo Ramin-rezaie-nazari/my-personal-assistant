267686094144274d22f81f25cf051b28039879aa
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-13 07:29:58 +0330

    feat(context): register life context priority resolver

--- apps/backend/src/modules/context-engine/context-engine.module.ts
+++ apps/backend/src/modules/context-engine/context-engine.module.ts
@@ -4,10 +4,11 @@ import { ContextEngineController } from './controllers/context-engine.controller
 import { ContextEngineService } from './services/context-engine.service';
 import { ContextBuilderService } from './services/context-builder.service';
 import { LifeContextFusionService } from './services/life-context-fusion.service';
+import { ContextPriorityResolverService } from './services/context-priority-resolver.service';
 
 @Module({
   controllers: [ContextEngineController],
-  providers: [ContextEngineService, ContextBuilderService, LifeContextFusionService],
-  exports: [ContextEngineService, ContextBuilderService, LifeContextFusionService],
+  providers: [ContextEngineService, ContextBuilderService, LifeContextFusionService, ContextPriorityResolverService],
+  exports: [ContextEngineService, ContextBuilderService, LifeContextFusionService, ContextPriorityResolverService],
 })
 export class ContextEngineModule {}
