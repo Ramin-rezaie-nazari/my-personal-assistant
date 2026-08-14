1f851320918f9716387c6374bdf79bc0c81023cd
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-14 08:10:40 +0330

    feat(shopping): register shopping module

---
+++ apps/backend/src/modules/shopping/shopping.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { InventoryModule } from '../inventory/inventory.module';
+import { ShoppingController } from './shopping.controller';
+import { ShoppingService } from './shopping.service';
+
+@Module({
+  imports: [InventoryModule],
+  controllers: [ShoppingController],
+  providers: [ShoppingService],
+})
+export class ShoppingModule {}
