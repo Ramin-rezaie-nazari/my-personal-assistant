27024702461f9d3883621a05a8785a95ef9813d4
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-14 08:14:29 +0330

    feat(recipes): register inventory matcher

--- apps/backend/src/modules/recipes/recipes.module.ts
+++ apps/backend/src/modules/recipes/recipes.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../common/database/prisma.module';
 import { RecipesController } from './controllers/recipes.controller';
 import { RecipesService } from './services/recipes.service';
+import { RecipeInventoryMatcherService } from './services/recipe-inventory-matcher.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [RecipesController],
-  providers: [RecipesService],
-  exports: [RecipesService],
+  providers: [RecipesService, RecipeInventoryMatcherService],
+  exports: [RecipesService, RecipeInventoryMatcherService],
 })
 export class RecipesModule {}
