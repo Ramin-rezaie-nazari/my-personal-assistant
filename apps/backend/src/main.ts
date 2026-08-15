ee9bd787515fc743b9269a9d0782fe973d77880d
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-15 19:42:58 +0330

    refactor(backend): use centralized bootstrap

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,21 +1,3 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AppConfigService } from './common/config/app-config/app-config.service';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  const configService = app.get(AppConfigService);
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  await app.listen(configService.port);
-}
+import { bootstrap } from './bootstrap';
 
 void bootstrap();
