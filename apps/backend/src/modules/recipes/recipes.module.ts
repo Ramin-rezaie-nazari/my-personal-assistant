ae85143bbd3409a56a7f7d8aa426334d818e288e
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-18 11:10:56 +0330

    feat(food): wire end-to-end food operating loop into recipes

--- apps/backend/src/modules/recipes/recipes.module.ts
+++ apps/backend/src/modules/recipes/recipes.module.ts
@@ -1,23 +1,28 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/database/prisma.module';
 import { NutritionModule } from '../nutrition/nutrition.module';
+import { ShoppingModule } from '../shopping/shopping.module';
+import { BudgetIntelligenceModule } from '../budget-intelligence/budget-intelligence.module';
 import { RecipesController } from './controllers/recipes.controller';
 import { RecipesService } from './services/recipes.service';
 import { RecipeInventoryMatcherService } from './services/recipe-inventory-matcher.service';
 import { GlobalCountryFoodService } from './services/global-country-food.service';
+import { FoodOperatingLoopService } from './services/food-operating-loop.service';
 
 @Module({
-  imports: [PrismaModule, NutritionModule],
+  imports: [PrismaModule, NutritionModule, ShoppingModule, BudgetIntelligenceModule],
   controllers: [RecipesController],
   providers: [
     RecipesService,
     RecipeInventoryMatcherService,
     GlobalCountryFoodService,
+    FoodOperatingLoopService,
   ],
   exports: [
     RecipesService,
     RecipeInventoryMatcherService,
     GlobalCountryFoodService,
+    FoodOperatingLoopService,
   ],
 })
 export class RecipesModule {}
