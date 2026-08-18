6ab1c5f27aeaeeb01b6e30f72e0a92edb9768c29
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-18 10:17:32 +0330

    feat(nutrition): wire recipe scaling service

--- apps/backend/src/modules/nutrition/nutrition.module.ts
+++ apps/backend/src/modules/nutrition/nutrition.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NutritionController } from './controllers/nutrition.controller';
 import { NutritionService } from './services/nutrition.service';
+import { RecipeServingScalingService } from './recipe-intelligence/recipe-serving-scaling.service';
 
 @Module({
   controllers: [NutritionController],
-  providers: [NutritionService],
-  exports: [NutritionService],
+  providers: [NutritionService, RecipeServingScalingService],
+  exports: [NutritionService, RecipeServingScalingService],
 })
 export class NutritionModule {}
