6b8c83dd93f779630e26c273fca135e3650e5917
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-08-18 10:18:36 +0330

    feat(recipes): connect recipe scaling intelligence

--- apps/backend/src/modules/recipes/recipes.module.ts
+++ apps/backend/src/modules/recipes/recipes.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../common/database/prisma.module';
+import { NutritionModule } from '../nutrition/nutrition.module';
 import { RecipesController } from './controllers/recipes.controller';
 import { RecipesService } from './services/recipes.service';
 import { RecipeInventoryMatcherService } from './services/recipe-inventory-matcher.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NutritionModule],
   controllers: [RecipesController],
   providers: [RecipesService, RecipeInventoryMatcherService],
   exports: [RecipesService, RecipeInventoryMatcherService],
