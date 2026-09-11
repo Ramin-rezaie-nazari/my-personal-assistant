3789bcc800791172e18e67ee2a8e91a65a5a3858
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-09-11 22:23:38 +0330

    remediation: remove empty Context Engine HTTP controller surface

--- apps/backend/src/modules/context-engine/context-engine.module.ts
+++ apps/backend/src/modules/context-engine/context-engine.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { ContextEngineController } from './controllers/context-engine.controller';
 import { ContextEngineService } from './services/context-engine.service';
 import { ContextBuilderService } from './services/context-builder.service';
 import { LifeContextFusionService } from './services/life-context-fusion.service';
 import { ContextPriorityResolverService } from './services/context-priority-resolver.service';
 
 @Module({
-  controllers: [ContextEngineController],
   providers: [
     ContextEngineService,
     ContextBuilderService,
