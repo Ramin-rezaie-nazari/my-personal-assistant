3bb9bf16c0352b2feec11bce4ff17790adc70cc7
Ramin-rezaie-nazari <ramin.rezaie12345@gmail.com>
2026-09-12 14:15:39 +0330

    test(mobile): map native storage modules in Jest

--- apps/mobile/jest.config.cjs
+++ apps/mobile/jest.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '.*\\.spec\\.ts$',
-  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleFileExtensions: ['js', 'json', 'ts', 'cjs'],
   testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^expo-secure-store$': '<rootDir>/test-mocks/expo-secure-store.cjs',
+    '^@react-native-async-storage/async-storage$': '<rootDir>/test-mocks/async-storage.cjs',
+  },
 };
